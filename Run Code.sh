2853b46d82173a289927c8755b0a8c3cbf441519
redlii <113053297+redlli@users.noreply.github.com>
2024-01-21 12:06:30 +0530

    Create Run Code.sh

---
+++ Run Code.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "$GEDIT_CURRENT_DOCUMENT_DIR" #to see current DIR
+echo "$GEDIT_CURRENT_DOCUMENT_LANGUAGE" # to see Language 
+
+qterminal -e "$GEDIT_CURRENT_DOCUMENT_LANGUAGE" "$GEDIT_CURRENT_DOCUMENT_NAME"  #this opens terminal and runs the coomand 
+
+#right now its only supports python 
