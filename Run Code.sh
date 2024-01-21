a213e2c4e24567e8f1be6fe99ea823ad6cdac928
redlii <113053297+redlli@users.noreply.github.com>
2024-01-21 12:29:09 +0530

    Update Run Code.sh

--- Run Code.sh
+++ Run Code.sh
@@ -6,3 +6,4 @@ echo "$GEDIT_CURRENT_DOCUMENT_LANGUAGE" # to see Language
 qterminal -e "$GEDIT_CURRENT_DOCUMENT_LANGUAGE" "$GEDIT_CURRENT_DOCUMENT_NAME"  #this opens terminal and runs the coomand 
 
 #right now its only supports python 
+#dont forgot to add blank input function 
